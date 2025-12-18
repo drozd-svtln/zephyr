47eab726f24c9e4ce9df281512359a7dedd106dd
Svitlana Drozd <svitlana_drozd@epam.com>
2025-12-18 19:44:55 +0200

    dump stack
    Signed-off-by: Svitlana Drozd <svitlana_drozd@epam.com>

--- subsys/disk/disk_access.c
+++ subsys/disk/disk_access.c
@@ -142,6 +142,7 @@ int disk_access_write(const char *pdrv, const uint8_t *data_buf,
 int disk_access_ioctl(const char *pdrv, uint8_t cmd, void *buf)
 {
 	LOG_ERR("MY_LOG %s %d pdrv %s", __FILE__, __LINE__, pdrv);
+	dump_stack();
 	struct disk_info *disk = disk_access_get_di(pdrv);
 	int rc = -EINVAL;
 	LOG_ERR("MY_LOG %s %d", __FILE__, __LINE__);
