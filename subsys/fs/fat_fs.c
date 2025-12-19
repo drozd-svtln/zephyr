e87c9edf55b43e161ccb548a4d861bf5893893e3
Svitlana Drozd <svitlana_drozd@epam.com>
2025-12-19 20:17:11 +0200

    some more fields
    Signed-off-by: Svitlana Drozd <svitlana_drozd@epam.com>

--- subsys/fs/fat_fs.c
+++ subsys/fs/fat_fs.c
@@ -455,6 +455,8 @@ static int fatfs_mount(struct fs_mount_t *mountp)
 	LOG_ERR("MY_LOG %*ph", (int)sizeof(*my_struct), my_struct);
 	LOG_ERR("MY_LOG my_struct->fs_type %d", my_struct->fs_type);
 	LOG_ERR("MY_LOG my_struct->pdrv %d", my_struct->pdrv);
+	LOG_ERR("MY_LOG my_struct->ldrv %d", my_struct->ldrv);
+	LOG_ERR("MY_LOG my_struct->n_fats %d", my_struct->n_fats);
 	LOG_ERR("MY_LOG %p", mountp->fs_data);
 	// LOG_ERR("MY_LOG ");
 	// LOG_ERR("MY_LOG ");
