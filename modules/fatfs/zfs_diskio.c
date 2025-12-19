1caa8c92f04606bec4390df647b08d3a9c1c42ab
Svitlana Drozd <svitlana_drozd@epam.com>
2025-12-19 19:02:34 +0200

    delete logs for array
    Signed-off-by: Svitlana Drozd <svitlana_drozd@epam.com>

--- modules/fatfs/zfs_diskio.c
+++ modules/fatfs/zfs_diskio.c
@@ -128,10 +128,6 @@ DRESULT disk_ioctl(BYTE pdrv, BYTE cmd, void *buff)
 		} else {
 			/* Power disk on */
 			printk("MY_LOG %s %d PDRV_STR_ARRAY[pdrv] %s pdrv %d\n\n", __FILE__, __LINE__, PDRV_STR_ARRAY[pdrv], pdrv);
-			printk("MY_LOG VolumeStr[0] %s\n", VolumeStr[0]);
-			printk("MY_LOG VolumeStr[1] %s\n", VolumeStr[1]);
-			printk("MY_LOG VolumeStr[2] %s\n", VolumeStr[2]);
-			printk("MY_LOG VolumeStr[3] %s\n\n", VolumeStr[3]);
 
 			printk("MY_LOG PDRV_STR_ARRAY[0] %s\n", PDRV_STR_ARRAY[0]);
 			printk("MY_LOG PDRV_STR_ARRAY[1] %s\n", PDRV_STR_ARRAY[1]);
