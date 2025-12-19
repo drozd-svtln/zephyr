218e675598d2ef9478b284cfebf46ff03b65fa3f
Svitlana Drozd <svitlana_drozd@epam.com>
2025-12-19 17:41:31 +0200

    manually def FF_VOLUMES as 5
    Signed-off-by: Svitlana Drozd <svitlana_drozd@epam.com>

--- modules/fatfs/zephyr_fatfs_config.h
+++ modules/fatfs/zephyr_fatfs_config.h
@@ -109,18 +109,22 @@
 #define _FF_DISK_NAME(node) DT_PROP(node, disk_name),
 
 #undef FF_VOLUME_STRS
-/*
 #define FF_VOLUME_STRS \
 	DT_FOREACH_STATUS_OKAY(zephyr_flash_disk, _FF_DISK_NAME) \
 	DT_FOREACH_STATUS_OKAY(zephyr_ram_disk, _FF_DISK_NAME) \
 	DT_FOREACH_STATUS_OKAY(zephyr_sdmmc_disk, _FF_DISK_NAME) \
 	DT_FOREACH_STATUS_OKAY(zephyr_mmc_disk, _FF_DISK_NAME) \
 	DT_FOREACH_STATUS_OKAY(st_stm32_sdmmc, _FF_DISK_NAME)
-*/
+
+/*
 #define FF_VOLUME_STRS "RAM","NAND","CF","SD","SD2","USB","USB2","USB3"
+#define FF_VOLUME_STRS "SD"
+*/
+
 
 #undef FF_VOLUMES
-#define FF_VOLUMES NUM_VA_ARGS_LESS_1(FF_VOLUME_STRS _)
+#define FF_VOLUMES 5
+// #define FF_VOLUMES NUM_VA_ARGS_LESS_1(FF_VOLUME_STRS _)
 
 #if defined(CONFIG_FS_FATFS_EXTRA_NATIVE_API)
 #undef FF_USE_LABEL
